8c8e8470c5cceb2f8222ee77cf8ad52f5b720830
ANDRESROJAS00 <andre.rojasm@duocuc.cl>
2023-11-19 20:42:37 -0300

    RegistroUsuario Completado v1

--- src/controlador/RegistroUsuario.java
+++ src/controlador/RegistroUsuario.java
@@ -1,10 +1,12 @@
-
 package controlador;
 
 import bd.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 import modelo.Usuario;
 
@@ -13,8 +15,7 @@ import modelo.Usuario;
  * @author andre
  */
 public class RegistroUsuario {
-    
-    
+
     public boolean agregarUsuario(Usuario usuario) {
         try {
 
@@ -28,7 +29,6 @@ public class RegistroUsuario {
             stmt.setString(3, usuario.getAppaterno());
             stmt.setString(4, usuario.getApmaterno());
             stmt.setInt(5, usuario.getNpi());
-            
 
             stmt.executeUpdate();
             stmt.close();
@@ -41,8 +41,7 @@ public class RegistroUsuario {
         }
 
     }
-    
-    
+
     public boolean eliminarUsuario(int idUsuario) {
 
         boolean flag = false;
@@ -70,19 +69,18 @@ public class RegistroUsuario {
         return flag;
 
     }
-    
-    
-    public boolean actualizarCarrera(String nombre, String nombreNew) {
+
+    public boolean actualizarUsuario(String idUsuario, String nuevoNombre) {
 
         try {
 
             Conexion con = new Conexion();
             Connection cnx = con.obtenerConexion();
 
-            String query = "UPDATE carrera SET nombre = ? WHERE nombre = ?";
+            String query = "UPDATE usuario SET nombre = ? WHERE id_usuario = ?";
             PreparedStatement stmt = cnx.prepareCall(query);
-            stmt.setString(1, nombreNew);
-            stmt.setString(2, nombre);
+            stmt.setString(1, nuevoNombre);
+            stmt.setString(2, idUsuario);
 
             stmt.executeUpdate();
             stmt.close();
@@ -95,5 +93,83 @@ public class RegistroUsuario {
         }
 
     }
+
+    public Usuario buscarPorNombre(String nombre) {
+
+        Usuario usuario = new Usuario();
+
+        try {
+
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            String query = "SELECT * FROM usuario WHERE nombre = ?";
+            PreparedStatement stmt = cnx.prepareCall(query);
+            stmt.setString(1, nombre);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                usuario.setId_usuario(rs.getInt("id_usuario"));
+                usuario.setNpi(rs.getInt("npi"));
+                usuario.setPnombre(rs.getString("pnombre"));
+                usuario.setSnombre(rs.getString("snombre"));
+                usuario.setAppaterno(rs.getString("appaterno"));
+                usuario.setApmaterno(rs.getString("apmaterno"));
+            }
+
+            rs.close();
+            stmt.close();
+            cnx.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error en la consulta SQL buscar por nombre, " + e.getMessage());
+
+        }
+        return usuario;
+
+    }
     
+    public List<Usuario> buscarTodos() {
+
+    List<Usuario> lista = new ArrayList<>();
+
+    try {
+
+        Conexion con = new Conexion();
+        Connection cnx = con.obtenerConexion();
+
+        String query = "SELECT * FROM usuario";
+        PreparedStatement stmt = cnx.prepareCall(query);
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            Usuario usuario = new Usuario();
+            usuario.setId_usuario(rs.getInt("id_usuario"));
+            usuario.setNpi(rs.getInt("npi"));
+            usuario.setPnombre(rs.getString("pnombre"));
+            usuario.setSnombre(rs.getString("snombre"));
+            usuario.setAppaterno(rs.getString("appaterno"));
+            usuario.setApmaterno(rs.getString("apmaterno"));
+
+            lista.add(usuario);
+        }
+
+        rs.close();
+        stmt.close();
+        cnx.close();
+
+    } catch (SQLException e) {
+        System.out.println("Error en la consulta SQL consultar todos los datos, " + e.getMessage());
+
+    }
+    return lista;
+}
+
+    
+    
+    
+    
+
 }
